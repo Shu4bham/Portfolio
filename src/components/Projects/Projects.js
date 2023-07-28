3842b5500e2cd7ceafc71ca1796dba01c3425bda
shubham <shubhamkamboj2550@gmail.com>
2023-07-28 22:26:05 +0530

    Added new project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -75,18 +75,6 @@ function Projects() {
               demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
             />
           </Col>
-
-          {/* <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/shu4bham/Face_And_Emotion_Detection"
-              // demoLink="https://shu4bham.live/"      <--------Please include a demo link here 
-            />
-          </Col> */}
         </Row>
       </Container>
     </Container>
